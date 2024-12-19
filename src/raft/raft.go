ea7d3f6a209c7b98deff9e16f15c4e3f97dd6ef2
pokeyyy <1489838471@qq.com>
2024-12-19 15:15:13 +0800

    #

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -18,6 +18,8 @@ package raft
 //
 
 import (
+	"bytes"
+	"encoding/gob"
 	"labrpc"
 	"math/rand"
 	"sync"
@@ -58,8 +60,8 @@ type Raft struct {
 	commitIndex int
 	lastApplied int
 
-	nextIndex  []int
-	matchIndex []int
+	nextIndex  []int //记录对于集群中每个节点，下一个需要发送给该节点的日志条目的索引位置。
+	matchIndex []int //记录对于集群中每个节点，目前已知该节点复制成功的日志条目中最高（最新）的索引位置。
 
 	state          int
 	electionTimer  *time.Timer
@@ -73,7 +75,10 @@ func (rf *Raft) GetState() (int, bool) {
 
 	var term int
 	var isleader bool
-	// Your code here.
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	term = rf.currentTerm
+	isleader = rf.state == 3
 	return term, isleader
 }
 
@@ -81,24 +86,22 @@ func (rf *Raft) GetState() (int, bool) {
 // where it can later be retrieved after a crash and restart.
 // see paper's Figure 2 for a description of what should be persistent.
 func (rf *Raft) persist() {
-	// Your code here.
-	// Example:
-	// w := new(bytes.Buffer)
-	// e := gob.NewEncoder(w)
-	// e.Encode(rf.xxx)
-	// e.Encode(rf.yyy)
-	// data := w.Bytes()
-	// rf.persister.SaveRaftState(data)
+	w := new(bytes.Buffer)
+	e := gob.NewEncoder(w)
+	e.Encode(rf.currentTerm)
+	e.Encode(rf.votedFor)
+	e.Encode(rf.log)
+	data := w.Bytes()
+	rf.persister.SaveRaftState(data)
 }
 
 // restore previously persisted state.
 func (rf *Raft) readPersist(data []byte) {
-	// Your code here.
-	// Example:
-	// r := bytes.NewBuffer(data)
-	// d := gob.NewDecoder(r)
-	// d.Decode(&rf.xxx)
-	// d.Decode(&rf.yyy)
+	r := bytes.NewBuffer(data)
+	d := gob.NewDecoder(r)
+	d.Decode(&rf.currentTerm)
+	d.Decode(&rf.votedFor)
+	d.Decode(&rf.log)
 }
 
 // example RequestVote RPC arguments structure.
@@ -146,6 +149,7 @@ type AppendEntriesArgs struct {
 	PrevLogIndex int
 	PreLogTerm   int
 	LeaderCommit int
+	Entries      []LogEntry
 }
 
 type AppendEntriesReply struct {
@@ -154,7 +158,24 @@ type AppendEntriesReply struct {
 }
 
 func (rf *Raft) AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if args.Term < rf.currentTerm {
+		reply.Term = rf.currentTerm
+		reply.Success = false
+		return
+	}
+	rf.currentTerm = args.Term
+	rf.votedFor = -1
+	rf.state = 1
+	rf.electionTimer.Reset(randTime())
+	rf.persist()
 
+	if args.PrevLogIndex < len(rf.log) {
+		reply.Term, response.Success = 0, false
+		return
+
+	}
 }
 
 // example code to send a RequestVote RPC to a server.
@@ -201,7 +222,10 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 // in Kill(), but it might be convenient to (for example)
 // turn off debug output from this instance.
 func (rf *Raft) Kill() {
-	// Your code here, if desired.
+	rf.mu.Lock()
+	rf.me = -1
+	rf.electionTimer.Reset(0)
+	rf.mu.Unlock()
 }
 
 // the service or tester wants to create a Raft server. the ports
@@ -221,34 +245,21 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.me = me
 
 	// Your initialization code here.
+	rf.nextIndex = make([]int, len(peers))
+	rf.matchIndex = make([]int, len(peers))
 	rf.currentTerm = 0
 	rf.state = 1
 	rf.votedFor = -1
 	rf.countvote = 0
 	rf.log = make([]LogEntry, 1)
 	rf.electionTimer = time.NewTimer(randTime())
-	for {
-		rf.mu.Lock()
-		switch rf.state {
-		case 1:
-			rf.mu.Unlock()
-			select {
-			case <-rf.electionTimer.C:
-				rf.mu.Lock()
-				rf.state = 2
-				rf.mu.Unlock()
-			}
-		case 2:
-			rf.mu.Lock()
-			rf.electionTimer.Reset(randTime())
-			rf.checktimeout()
-		}
-	}
+	rf.heartbeatTimer = time.NewTimer(randTime())
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
-
-	go rf.Election()
+	//rf.applyCond = sync.NewCond(&rf.mu)
+	//lastLog := rf.getLastLog()
+	go rf.checktimeout()
 
 	return rf
 }
@@ -281,12 +292,10 @@ func randTime() time.Duration {
 }
 
 func (rf *Raft) Election() {
-	rf.mu.Lock()
-	rf.currentTerm++
-	rf.votedFor = rf.me
-	rf.countvote = 1
-
 	args := RequestVoteArgs{Term: rf.currentTerm, CandidateId: rf.me}
+	rf.countvote = 1
+	rf.votedFor = rf.me
+	//rf.persist()
 	for i := 0; i < len(rf.peers); i++ {
 		if i == rf.me {
 			continue
@@ -294,12 +303,22 @@ func (rf *Raft) Election() {
 		go func(server int) {
 			reply := RequestVoteReply{}
 			if rf.state == 2 && rf.sendRequestVote(i, args, &reply) {
-				if reply.VoteGranted {
+				rf.mu.Lock()
+				defer rf.mu.Unlock()
+
+				if rf.currentTerm == reply.Term && reply.VoteGranted {
 					rf.countvote++
+					if rf.countvote > len(rf.peers)/2 {
+						DPrintf("{Node %v} receives majority votes in term %v", rf.me, rf.currentTerm)
+						rf.state = 3
+						//rf.BroadcastHeartbeat(true)
+					}
 				} else {
 					if reply.Term > rf.currentTerm {
 						rf.currentTerm = reply.Term
 						rf.state = 1
+						rf.votedFor = -1
+						//rf.persist()
 					}
 				}
 			}
